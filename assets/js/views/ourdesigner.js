f259fdfdb303c6833c670f7955ce01f0bd188cea
sorrymeika <sorrymeika@163.com>
2014-11-17 07:37:14 +0800

    fx

---
+++ assets/js/views/ourdesigner.js
@@ -0,0 +1,32 @@
+﻿define(['$','ui/sl','app','sl/widget/loading'],function (require,exports,module) {
+    var $=require('$'),
+        sl=require('sl/sl'),
+        Loading=require('sl/widget/loading'),
+        app=require('app'),
+        util=require('util');
+
+    module.exports=sl.Activity.extend({
+        template: 'views/ourdesigner.html',
+        events: {
+            'tap .js_back': 'back'
+        },
+        onCreate: function () {
+            var that=this;
+
+            that.$list=that.$('.js_list');
+            that.loading=new Loading(that.$list);
+            that.loading.load({
+                url: '/json/about/designer',
+                success: function (res) {
+                    that.$list.html(that.tmpl('list',res));
+                }
+            });
+        },
+        onStart: function () {
+        },
+        onResume: function () {
+        },
+        onDestory: function () {
+        }
+    });;
+});
