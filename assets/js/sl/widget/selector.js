89824905b5870b04cfc5c9d6e6fa33ecfaf6e1fc
sorrymeika <sorrymeika@163.com>
2015-01-05 00:03:06 +0800

    fx

--- assets/js/sl/widget/selector.js
+++ assets/js/sl/widget/selector.js
@@ -1,11 +1,11 @@
-﻿define(['$','./../base','./../view','./../tmpl','./touch'],function(require,exports,module) {
+﻿define(['$','./../base','./../view','./../tmpl','./touch'],function (require,exports,module) {
     var $=require('$'),
         Touch=require('./touch');
 
     tmpl=require('./../tmpl');
 
     var selector=Touch.extend({
-        start: function() {
+        start: function () {
             var that=this;
             that.y=that.con.scrollTop;
             that.startY=that.y;
@@ -16,23 +16,25 @@
             return true;
         },
         minDelta: 0,
-        move: function(x,y) {
+        move: function (x,y) {
             var that=this;
             that.con.scrollTop=y;
         },
 
-        _startAni: function(x,y,duration) {
-            y=y-y%22;
+        _startAni: function (x,y,duration) {
+            var a=y%26;
+            y=y-(a>20?a-26:a);
             Touch.prototype._startAni.call(this,x,y,duration);
         },
 
-        end: function() {
+        end: function () {
             var y=this.y;
-            y=y-y%22;
+            var a=y%26;
+            y=y-(a>20?a-26:a);
             if(this.y!=y) {
-                this._moving(0,y,100);
+                this._moving(0,y,200);
             }
-            var index=y/22;
+            var index=Math.round(y/26);
             this.trigger('Change',[index,this.data[index]]);
         },
 
@@ -40,14 +42,13 @@
 
         template: tmpl('<li>${text}</li>'),
 
-        initialize: function() {
+        initialize: function () {
             var that=this;
             var options=this.options;
             var data=options.data||[];
             !data.length&&data.push({ text: '无数据' });
 
             options.onChange&&this.on("Change",options.onChange);
-
 
             this.$con=this.$('.selectorcon');
             this.con=this.$con[0];
@@ -57,10 +58,10 @@
             this.render(data);
         },
 
-        render: function(data) {
+        render: function (data) {
             var that=this;
             var html=[];
-            $.each(data,function(i,item) {
+            $.each(data,function (i,item) {
                 html.push(that.template(item));
             });
 
@@ -69,7 +70,7 @@
         }
     });
 
-    var Selector=function(options) {
+    var Selector=function (options) {
         options=$.extend({
             data: []
         },options);
@@ -79,21 +80,49 @@
 
         !$.isArray(data[0])&&(data=[data]);
 
-        this.$el=$('<div class="selectorwrap"></div>').appendTo(document.body);
+        this.$el=$('<div class="selectorwrap"><div class="selectorbar"><b class="js_click">完成</b></div></div>').appendTo(document.body);
         this.selectors=[];
 
-        $.each(data,function(i,item) {
+        $.each(data,function (i,item) {
             that.render(item);
+        });
+
+        this.$el.on("click",'.js_click',function () {
+            that.hide();
         });
     };
 
     Selector.prototype={
-        render: function(data) {
+        _visible: true,
+        render: function (data) {
             var sel=new selector({
                 data: data
             });
             this.selectors.push(sel);
             this.$el.append(sel.$el);
+        },
+        hide: function () {
+            var that=this;
+            this._visible&&this.$el.css({
+                '-webkit-transform': 'translate(0px,0%)'
+            })
+            .animate({
+                'translate': '0px,100%'
+            },300,'ease-out',function () {
+                that._visible=false;
+                $(this).hide();
+            })
+        },
+        show: function () {
+            !this._visible&&this.$el.css({
+                'display': 'block',
+                '-webkit-transform': 'translate(0px,100%)'
+            })
+            .animate({
+                'translate': '0px,0%'
+            },300,'ease-out',function () {
+                that._visible=true;
+            });
         }
     };
 
