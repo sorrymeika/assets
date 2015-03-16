86026012442f5e9f09911b23445d9b31e4142184
sun.lu <sun.lu@Ctrip.com>
2015-03-16 16:14:15 +0800

    fx

--- assets/js/tools/tools.js
+++ assets/js/tools/tools.js
@@ -1,4 +1,4 @@
-﻿define(function (require) {
+﻿define(function(require) {
     var $=require('$');
 
     require('./uglify');
@@ -11,7 +11,7 @@
     head.insertBefore(meta,head.firstChild);
 
     var bridge=require('bridge');
-    bridge.url=function (url) {
+    bridge.url=function(url) {
         return /^http\:\/\//.test(url)?url:('index.cshtml?path='+encodeURIComponent(url));
     };
 
@@ -37,7 +37,7 @@
         global_defs: {}
     });
 
-    var parse=function (code) {
+    var parse=function(code) {
         var ast=UglifyJS.parse(code);
         ast.figure_out_scope();
         ast=ast.transform(compressor);
@@ -48,9 +48,9 @@
         return code;
     };
 
-    var replaceDefine=function (id,code) {
+    var replaceDefine=function(id,code) {
 
-        return code.replace(/^\s*define\(([^\(]+?,){0,1}function/,function (r0,p) {
+        return code.replace(/^\s*define\(([^\(]+?,){0,1}function/,function(r0,p) {
 
             p=eval('['+(p||'')+']');
             typeof p[0]==='string'?(p[0]=id):p.splice(0,0,id);
@@ -63,23 +63,23 @@
     var tools={
         times: 0,
 
-        finish: function (path,text) {
+        finish: function(path,text) {
             if(this.times==0) {
                 $.post('tools.cshtml?action=finish',{
                     path: path,
                     text: text
 
-                },function (res) {
+                },function(res) {
                     console.log(res);
                 });
             }
         },
 
-        ajax: function (url,data) {
+        ajax: function(url,data) {
             var that=this;
             that.times++;
 
-            $.post(url,data,function (res) {
+            $.post(url,data,function(res) {
                 console.log(url+' '+res);
 
                 that.times--;
@@ -87,19 +87,19 @@
             });
         },
 
-        save: function (path,text) {
+        save: function(path,text) {
             this.ajax('tools.cshtml?action=save',{
                 path: path,
                 text: text
             });
         },
 
-        js: function (path) {
+        js: function(path) {
             var that=this;
 
-            $.each(path,function (i,items) {
+            $.each(path,function(i,items) {
                 var result=[];
-                var combine=function () {
+                var combine=function() {
 
                     if(!items.length) {
                         var code=result.join('\n');
@@ -111,7 +111,7 @@
                     var id=items.shift();
                     var url=seajs.resolve(id);
 
-                    $.get(url+'?'+new Date().getTime(),function (res) {
+                    $.get(url+'?'+new Date().getTime(),function(res) {
 
                         res=parse(replaceDefine(id,res));
 
@@ -127,24 +127,25 @@
             return this;
         },
 
-        html: function (path,js,api) {
+        html: function(path,js,api) {
             var api='<meta name="api-base-url" content="'+api+'" />';
+            var now=new Date().getTime();
             var that=this,
                 scriptOpen='<script src="js/',
-                scriptClose='.js"></script>';
+                scriptClose='.js?v='+now+'"></script>';
 
-            $.each(path,function (i,url) {
-                $.get(url+'?'+new Date().getTime(),function (res) {
+            $.each(path,function(i,url) {
+                $.get(url+'?'+new Date().getTime(),function(res) {
                     res=res.replace(/<script[^>]+debug[^>]*>[\S\s]*?<\/script>/img,'')
                             .replace('<head>','<head>'+api)
-                            .replace(/<script[^>]*>([\S\s]*?)<\/script>/img,function (r0,r1) {
+                            .replace(/<script[^>]*>([\S\s]*?)<\/script>/img,function(r0,r1) {
                                 if(!$.trim(r1)) return r0;
                                 return '<script>'+parse(r1)+'</script>';
                             });
 
                     if(js) {
-                        var list=$.isArray(js)?js:(function (arr) {
-                            $.each(js,function (k) { arr.push(k); });
+                        var list=$.isArray(js)?js:(function(arr) {
+                            $.each(js,function(k) { arr.push(k); });
                             return arr;
                         })([]);
 
@@ -157,30 +158,30 @@
             return this;
         },
 
-        resource: function (resource) {
+        resource: function(resource) {
             this.ajax('tools.cshtml?action=resource',{
                 resource: resource.join(',')
 
             });
         },
 
-        compress: function (path) {
+        compress: function(path) {
 
             var that=this;
 
-            $.each(path,function (i,url) {
+            $.each(path,function(i,url) {
                 var index=url.lastIndexOf('.');
                 var ext=index== -1?'.js':url.substr(index);
 
                 if(ext==='.js') {
                     var id=url.replace(/\.js$/,'');
                     url="js/"+url;
-                    $.get(url+'?'+new Date().getTime(),function (res) {
+                    $.get(url+'?'+new Date().getTime(),function(res) {
                         res=parse(replaceDefine(id,res));
                         that.save(url,res);
                     });
                 } else if(ext==='.css') {
-                    $.get(url+'?'+new Date().getTime(),function (res) {
+                    $.get(url+'?'+new Date().getTime(),function(res) {
                         res=res.replace(/\s*([;|\:|,|\{|\}])\s*/img,'$1').replace(/[\r\n]/mg,'')
                                 .replace(/;}/mg,'}');
                         that.save(url,res);
@@ -193,7 +194,7 @@
         },
 
 
-        template: function (template) {
+        template: function(template) {
             var that=this;
 
             this.ajax('tools.cshtml?action=template',{
@@ -201,7 +202,7 @@
             });
         },
 
-        build: function (options) {
+        build: function(options) {
             options.js&&this.js(options.js);
             options.html&&this.html(options.html,options.js,options.api);
             options.resource&&this.resource(options.resource);
